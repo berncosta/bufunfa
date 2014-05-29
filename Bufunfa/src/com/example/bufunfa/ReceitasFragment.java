560e1ce17550eee3852d7f15eeaaea1eff896373
berncosta <bmcosta@192.168.0.20>
2014-05-28 23:40:49 -0300

    Signed-off-by: berncosta <bmcosta@192.168.0.20>

--- Bufunfa/src/com/example/bufunfa/ReceitasFragment.java
+++ Bufunfa/src/com/example/bufunfa/ReceitasFragment.java
@@ -1,11 +1,27 @@
 package com.example.bufunfa;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class ReceitasFragment extends Fragment {
 
 	public ReceitasFragment() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+		
+		View rootView = inflater.inflate(R.layout.fragment_receitas, container, false);
+		
+		TextView textView = (TextView) rootView.findViewById(R.id.text_label);
+		textView.setText("Hello World, Receitas!");
+		
+		return rootView; //alterar
 	}
 
 }
